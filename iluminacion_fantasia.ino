661e4b2d5e92ba76a0273146bef47ea2038b39ec
Mrtony94 <35722879+Mrtony94@users.noreply.github.com>
2021-05-09 00:45:54 +0200

    Update iluminacion_fantasia.ino

--- iluminacion_fantasia.ino
+++ iluminacion_fantasia.ino
@@ -1,9 +1,8 @@
-
 #include <Adafruit_NeoPixel.h>
 
 #define TRIG 4
 #define ECHO 5
-int num_leds = 5;
+int num_leds = 8;
 Adafruit_NeoPixel tira=Adafruit_NeoPixel (num_leds, 2, NEO_GRB+NEO_KHZ800);
 
 long duracion, distancia;
@@ -27,20 +26,40 @@ void loop(){
   distancia = duracion/58,3;
   Serial.println(distancia);
 
-  if(distancia <= 5){
-    tira.clear();   //Apagar los leds, por si había alguna encendida
+  if(distancia <= 25){
+       //Apagar los leds, por si había alguna encendida
     tira.setBrightness(100);    //Intensidad del color
-    tira.setPixelColor(0,255,255,255);    //Led número 0, color blanco
-    tira.setPixelColor(1,255,255,255);    //Led número 1, color blanco
+    tira.setPixelColor(0,255,255,0);    //Led número 0, color blanco
+    tira.setPixelColor(1,255,255,0);    //Led número 1, color blanco
     //...
     tira.show();
   }
-  if(distancia > 5 && distancia <= 10){
-    tira.clear();  
+  if(distancia > 25 && distancia <= 50){
+      
     tira.setBrightness(100);  
-    tira.setPixelColor(2,255,255,255);
-    tira.setPixelColor(3,255,255,255);
+    tira.setPixelColor(2,255,0,255);
+    tira.setPixelColor(3,255,0,255);
      //...
+    tira.show();
+  }
+  if(distancia > 50 && distancia <= 70){
+      
+    tira.setBrightness(100);  
+    tira.setPixelColor(4,0,0,255);
+    tira.setPixelColor(5,0,0,255);
+     //...
+    tira.show();
+  }
+  if(distancia > 70 && distancia <= 90){
+      
+    tira.setBrightness(100);  
+    tira.setPixelColor(6,255,0,255);
+    tira.setPixelColor(7,255,0,255);
+     //...
+    tira.show();
+  }
+  if (distancia > 90){
+    tira.clear();
     tira.show();
   }
 }
