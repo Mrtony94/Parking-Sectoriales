1a10b6df2a07d435ba34d7d3a55762fa078ce71a
Mrtony94 <35722879+Mrtony94@users.noreply.github.com>
2021-05-11 18:33:55 +0200

    Update sensor_fuego.ino

--- sensor_fuego.ino
+++ sensor_fuego.ino
@@ -1,21 +1,13 @@
-int buzzer = 11;      // selecciona el pin para el zumbador
-int valorSensor = 0;  // variable para almacenar el valor proveniente del sensor
+int BUZZER = 13.;      // selecciona el pin para el zumbador
+int FLAME = 2;  // selecciona el pin del sensor
 void setup() {
  //Seteo de la velocidad del puerto serial
  Serial.begin(9600);
  // declarar buzzer como una SALIDA:
-  pinMode(buzzer, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(FLAME, INPUT);
 }
 void loop() {
-  // leer el valor del sensor:
-  valorSensor = analogRead(A0);
-   Serial.println(valorSensor);
-  // activa el buzzer
-  if (valorSensor < 500){
-   digitalWrite(buzzer, HIGH);
-   delay(100);
-  // Desactiva el buzzer
-  digitalWrite(buzzer, LOW);
-  delay(50);  
-    }
+  
+  digitalWrite(BUZZER,digitalRead(FLAME));
  }
