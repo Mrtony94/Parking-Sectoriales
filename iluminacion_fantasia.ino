86bdc53c62c5ef4b733ea335b3fedcf9afac6932
davidgil014 <62967733+davidgil014@users.noreply.github.com>
2021-06-06 12:40:14 +0200

    ILUMINACION TERMINADA

--- iluminacion_fantasia.ino
+++ iluminacion_fantasia.ino
@@ -2,7 +2,7 @@
 
 #define TRIG 4
 #define ECHO 5
-int num_leds = 8;
+int num_leds = 28;
 Adafruit_NeoPixel tira=Adafruit_NeoPixel (num_leds, 2, NEO_GRB+NEO_KHZ800);
 
 long duracion, distancia;
@@ -26,40 +26,60 @@ void loop(){
   distancia = duracion/58,3;
   Serial.println(distancia);
 
-  if(distancia <= 25){
-       //Apagar los leds, por si había alguna encendida
-    tira.setBrightness(100);    //Intensidad del color
-    tira.setPixelColor(0,255,255,0);    //Led número 0, color blanco
-    tira.setPixelColor(1,255,255,0);    //Led número 1, color blanco
-    //...
-    tira.show();
-  }
-  if(distancia > 25 && distancia <= 50){
-      
-    tira.setBrightness(100);  
-    tira.setPixelColor(2,255,0,255);
-    tira.setPixelColor(3,255,0,255);
-     //...
-    tira.show();
-  }
-  if(distancia > 50 && distancia <= 70){
-      
-    tira.setBrightness(100);  
-    tira.setPixelColor(4,0,0,255);
-    tira.setPixelColor(5,0,0,255);
-     //...
-    tira.show();
-  }
-  if(distancia > 70 && distancia <= 90){
-      
-    tira.setBrightness(100);  
-    tira.setPixelColor(6,255,0,255);
-    tira.setPixelColor(7,255,0,255);
-     //...
-    tira.show();
-  }
-  if (distancia > 90){
+  if (distancia > 41) {
     tira.clear();
     tira.show();
   }
+  
+  if(distancia > 32 && distancia <= 40){
+      
+    tira.setBrightness(100);  
+    tira.setPixelColor(3,0,0,255);
+    tira.setPixelColor(4,0,0,255);
+    tira.setPixelColor(5,0,0,255);
+    tira.setPixelColor(6,0,0,255);
+    tira.setPixelColor(7,0,0,255);
+    tira.setPixelColor(8,0,0,255);
+     //...
+    tira.show();
+  }
+  if(distancia > 24 && distancia <= 32){
+      
+    tira.setBrightness(100);  
+    tira.setPixelColor(9,0,0,255);
+    tira.setPixelColor(10,0,0,255);
+    tira.setPixelColor(11,0,0,255);
+    tira.setPixelColor(12,0,0,255);
+    tira.setPixelColor(13,0,0,255);
+    tira.setPixelColor(14,0,0,255);
+     //...
+    tira.show();
+  }
+  if(distancia > 16 && distancia <= 24){
+      
+    tira.setBrightness(100);  
+    tira.setPixelColor(15,0,0,255);
+    tira.setPixelColor(16,0,0,255);
+    tira.setPixelColor(17,0,0,255);
+    tira.setPixelColor(18,0,0,255);
+    tira.setPixelColor(19,0,0,255);
+    tira.setPixelColor(20,0,0,255);
+     //...
+    tira.show();
+  }
+  if(distancia > 0 && distancia <= 16){
+      
+    tira.setBrightness(100);  
+    tira.setPixelColor(21,0,0,255);
+    tira.setPixelColor(22,0,0,255);
+    tira.setPixelColor(23,0,0,255);
+    tira.setPixelColor(24,0,0,255);
+    tira.setPixelColor(25,0,0,255);
+    tira.setPixelColor(26,0,0,255);
+    tira.setPixelColor(27,0,0,255);
+    
+     //...
+    tira.show();
+  }
+
 }
